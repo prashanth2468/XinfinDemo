3e3b05840d556d3fa49d8c3e5608f2398fa5da70
Peddapally Prashanth <prashanth@leewayhertz.com>
2021-06-02 18:44:59 +0530

    all the code

--- CXExample/CXExample/ViewController.swift
+++ CXExample/CXExample/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         XinFinValidator.helloXinFin()
+        let xinfinAPI = XinFinMethods()
+         xinfinAPI.getTransactionDetails(txHash: "0xd2d93b7617438534f48e078c3df0a8734e58af9bff9d1ab62697520855a2138d", success: { (successData) in
+             print(successData.hashValue)
+             
+         })
         
         // Do any additional setup after loading the view.
     }
